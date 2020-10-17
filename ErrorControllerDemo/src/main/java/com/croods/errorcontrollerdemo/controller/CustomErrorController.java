91a6566d84556e0bbb5eb091e749fab3a24f3d46
harshcroods <harsh.croods@gmail.com>
2020-10-17 14:34:41 +0530

    save default error message in database.

--- ErrorControllerDemo/src/main/java/com/croods/errorcontrollerdemo/controller/CustomErrorController.java
+++ ErrorControllerDemo/src/main/java/com/croods/errorcontrollerdemo/controller/CustomErrorController.java
@@ -21,26 +21,23 @@ public class CustomErrorController implements ErrorController {
 	@GetMapping("/error")
 	public String handleError(HttpServletRequest request) {
 		String errorPage = "error";
-		String errorMessage = "";
 
 		Integer status = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		String errorPath = (String) request.getAttribute(RequestDispatcher.ERROR_REQUEST_URI);
-
+		Throwable ex = (Throwable) request.getAttribute(RequestDispatcher.ERROR_EXCEPTION);
+		String errorMessage = ex.getMessage();
 		if (status != null) {
 			if (status == HttpStatus.NOT_FOUND.value()) {
 				// handle HTTP 404 Not Found error
 				errorPage = "error-404";
-				errorMessage = "404 | NOT FOUND";
 
 			} else if (status == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
 				// handle HTTP 500 Internal Server Error
 				errorPage = "error-500";
-				errorMessage = "500 Internal Server Error";
 
 			} else if (status == HttpStatus.FORBIDDEN.value()) {
 				// handle HTTP 403 Forbidden
 				errorPage = "error-403";
-				errorMessage = "403 Forbidden";
 			}
 		}
 		ErrorEntity errorDetails = new ErrorEntity();
